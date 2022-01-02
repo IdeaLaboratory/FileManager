1e585a87c0ce5e0aff4b50a15b568cfb48857f60
AdiTCG <adithecomputerguy@gmail.com>
2022-01-02 23:09:55 +0530

    Created folders for Views and ViewModels

---
+++ FileManager/ViewModels/FileManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.ViewModels
+{
+    /// <summary>
+    /// This is the main\root ViewModel
+    /// </summary>
+    public class FileManager
+    {
+
+    }
+}
